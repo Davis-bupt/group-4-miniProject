6c8cbec82d29ad55e6b0aaebd29417d4552f3432
testname <testemail.com>
2026-07-23 13:38:27 +0800

    add health endpoint

---
+++ src/main/java/com/deskflow/DeskflowApiApplication.java
@@ -0,0 +1,12 @@
+package com.deskflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskflowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeskflowApiApplication.class, args);
+    }
+}
